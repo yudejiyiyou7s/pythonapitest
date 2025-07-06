307800c3f304b0cbc0cd5b6a15882ee45ed0d581
yudejiyiyou7s <138568306+yudejiyiyou7s@users.noreply.github.com>
2025-07-06 15:15:53 +0800

    Delete python-test/pytestdemo directory

--- python-test/pytestdemo/run.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-import pytest
-import os
-if __name__ == "__main__":
-    pytest.main()
-    shutil.copy(r'D:\pycharm\code\python-test\pytestdemo\environment.xml',
-                r'D:\pycharm\code\python-test\pytestdemo\testcase\Login\allure-results')
-    os.system(f'allure serve D:/pycharm/code/python-test/pytestdemo/testcase/Login/allure-results')
